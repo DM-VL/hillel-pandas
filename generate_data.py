7bdb27cf8c1a8c74304c027aed352b95458d6503
Dmytro Volkov <v.d.a.9070@gmail.com>
2023-05-28 19:24:47 +0300

    pandas/hw12

--- generate_data.py
+++ generate_data.py
@@ -5,40 +5,33 @@ import pandas as pd
 import factory
 
 
-class StudentFactory(Factory):
+class CompanyFactory(Factory):
     class Meta:
         model = dict
 
     first_name = factory.Faker("first_name")
     last_name = factory.Faker('last_name')
-    gender = factory.Faker('random_element', elements=('M', 'F'))
-    email = factory.Faker('email')
-    mark_1 = factory.Faker('random_int', min=0, max=100)
-    mark_2 = factory.Faker('random_int', min=0, max=100)
-    mark_3 = factory.Faker('random_int', min=0, max=100)
-    attendance = factory.Faker('random_int', min=0, max=100)
-    height = factory.Faker('random_int', min=100, max=200)
-    weight = factory.Faker('random_int', min=30, max=100)
-    city = factory.Faker('random_element', elements=('Lviv', 'Kyiv', 'Kharkiv', 'Dnipro', 'Odesa'))
+    age = factory.Faker('random_int', min=20, max=45)
+    position = factory.Faker('random_element', elements=(
+        'junior', 'middle', 'senior', 'team_lead', 'tech_lead'))
+    salary = factory.Faker('random_int', min=20000, max=45000)
+    work_experience = factory.Faker('random_int', min=1, max=15)
 
 
-num_rows = 100
+num_rows = 50
 
-students = StudentFactory.create_batch(num_rows)
+employees = CompanyFactory.create_batch(num_rows)
 
-for student in students:
+for employee in employees:
     if random() < 0.1:
-        student['first_name'] = None
+        employee['first_name'] = None
     if random() < 0.1:
-        student['mark_3'] = None
+        employee['position'] = None
     if random() < 0.1:
-        student['city'] = None
+        employee['salary'] = -1 * employee['salary']
 
 
-df = pd.DataFrame(students)
-
-# df.to_json('students_records.json', orient='records', indent=4)
-# df.to_clipboard()
-df.to_xml('students_records.xml')
+df = pd.DataFrame(employees)
+df.to_csv('employees.csv', index=False)
 
 print(df)
